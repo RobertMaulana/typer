c45847ebe150cc10429682a04ea87ff323fcd97e
maulana <maulana.robert.mr@gmail.com>
2017-07-13 21:24:42 +0700

    jawab soal no 4, 5, 6, 7

--- typer.js
+++ typer.js
@@ -72,11 +72,11 @@ var TyperView = Backbone.View.extend({
 				'border-radius':'4px',
 				position:'absolute',
 				bottom:'0',
-				'min-width':'80%',
-				width:'80%',
+				'min-width':'60%',
+				width:'60%',
 				'margin-bottom':'10px',
 				'z-index':'1000'
-			}).keyup(function() {
+			}).keyup(function(e) {
 				var words = self.model.get('words');
 				for(var i = 0;i < words.length;i++) {
 					var word = words.at(i);
@@ -86,12 +86,97 @@ var TyperView = Backbone.View.extend({
 						word.set({highlight:typed_string.length});
 						if(typed_string.length == string.length) {
 							$(this).val('');
+							self.model.defaults.score += 5;
 						}
 					} else {
 						word.set({highlight:0});
 					}
 				}
+
+				/*---- win score & scoring -----*/
+				if (self.model.defaults.score >=100) {
+					self.model.winner()
+				}
+
+				if (typed_string !== undefined) {
+					if (typed_string.length > 0) {
+						var check = 0;
+						for (var i = 0; i < words.length; i++) {
+							var word_pos = words.at(i);
+							if (word_pos.get('highlight') !== 0) {
+								check++;
+							}
+						}
+						/*--------Punishment---------*/
+						if (e.keyCode >= 65 && e.keyCode <= 90) {
+							if (check === 0) {
+								self.model.defaults.score -= 1;
+							}
+						}
+						/*-------------------------------*/
+					}
+				}
+				/*-------------------------------*/
 			});
+
+		/*------ Button ---------*/
+
+		var btn_wrapper = $('<div>').css({
+													position: 'absolute',
+													bottom: '10px',
+													left: '10px',
+													'z-index': '9999'
+											});
+
+		var start_btn = $('<button>')
+										.addClass('btn btn-success')
+										.text('Start')
+										.css({'left': '10px'})
+										.click(function(){
+												self.model.start();
+												$(this).css({display: 'none'});
+												stop_btn.css({display: 'inline'});
+												pause_btn.css({display: 'inline'});
+										});
+		var stop_btn = $('<button>')
+										.addClass('btn btn-danger')
+										.text('Stop')
+										.css({display: 'none'})
+										.click(function(){
+												self.model.checkScore();
+												self.model.stop();
+												$(this).css({display: 'none'});
+												start_btn.css({display: 'inline'});
+												$('.word').remove();
+												self.model.defaults.score = 0;
+										});
+		var pause_btn = $('<button>')
+										.addClass('btn btn-default')
+										.text('Pause')
+										.css({'margin-left': '5px', 'display': 'none'})
+										.click(function(){
+												self.model.pause();
+												resume_btn.css({display: 'inline'});
+												$(this).css({display: 'none'});
+										});
+		var resume_btn = $('<button>')
+										.addClass('btn btn-default')
+										.text('Resume')
+										.css({'margin-left': '5px', 'display': 'none'})
+										.click(function(){
+												self.model.start();
+												pause_btn.css({display: 'inline'});
+												$(this).css({display: 'none'});
+										});
+		var score_wrapper = $('<div>').css({
+													position: 'absolute',
+													right: '40px',
+													top: '5px',
+													'z-index': 999
+												});
+		/*-------------------------------*/
+
+		var score = $('<h1>').html('Score : <span id="scores">' + self.model.defaults.score + '</span>');
 
 		$(this.el)
 			.append(wrapper
@@ -102,7 +187,15 @@ var TyperView = Backbone.View.extend({
 					.submit(function() {
 						return false;
 					})
-					.append(text_input)));
+					.append(text_input))
+					.append(btn_wrapper
+							.append(start_btn)
+							.append(stop_btn)
+							.append(pause_btn)
+							.append(resume_btn))
+	.append(score_wrapper
+		.append(score))
+				);
 
 		text_input.css({left:((wrapper.width() - text_input.width()) / 2) + 'px'});
 		text_input.focus();
@@ -113,6 +206,8 @@ var TyperView = Backbone.View.extend({
 	render: function() {
 		var model = this.model;
 		var words = model.get('words');
+
+		$('#scores').text(model.defaults.score);
 
 		for(var i = 0;i < words.length;i++) {
 			var word = words.at(i);
@@ -140,6 +235,8 @@ var Typer = Backbone.Model.extend({
 		min_speed:1,
 		/*- Trailing comma error in IE <9 -*/
 		max_speed:5, //<--
+		score: 0,
+		temp_typed_string: ''
 	},
 
 	initialize: function() {
@@ -149,6 +246,10 @@ var Typer = Backbone.Model.extend({
 		});
 	},
 
+	virtualStart: function() {
+
+	},
+
 	start: function() {
 
 		/*-optimize for animation and runs as fast as we display allow (~60 fps)-*/
@@ -156,15 +257,48 @@ var Typer = Backbone.Model.extend({
 		var self = this;
 
 		/*-tell browser to use animation frame-*/
-		window.requestAnimationFrame(function(){
+		self.startAnimate = window.requestAnimationFrame(function(){
 			setInterval(function() {
 				self.iterate();
 			},animation_delay);
 		})
+
+	},
+
+	stop: function () {
+		var self = this;
+		location.reload()
+	},
+
+	pause: function() {
+		var self = this;
+		clearInterval(self.startAnimate);
+	},
+
+	checkScore: function() {
+		alert('Score anda adalah: ' + this.defaults.score)
+	},
+
+	winner: function() {
+		alert('Anda menang dengan score!\n'+this.defaults.score+'\nPermainan akan diulang dari awal')
+		location.reload();
 	},
 
 	iterate: function() {
+
 		var words = this.get('words');
+
+		/*- Arrangement of letters -*/
+		$(window).on('resize', function(){
+			for(var i = 0;i < words.length;i++) {
+				var word_width = word.get('string').length * 25;
+				if (words.at(i).get('x') + word_width > $(window).width()){
+					words.at(i).set({x: $(window).width() - word_width});
+				}
+			}
+		});
+		/*---------------------------*/
+
 		if(words.length < this.get('max_num_words')) {
 			var top_most_word = undefined;
 			for(var i = 0;i < words.length;i++) {
